3d51d07b5350e5a90efed3f203ce076c52052b85
Diego <diegoaribeiro1910@gmail.com>
2024-05-26 21:10:56 -0300

    mudanças

--- AlmoxarifadoAPI/Program.cs
+++ AlmoxarifadoAPI/Program.cs
@@ -19,11 +19,49 @@ builder.Services.AddScoped<IGrupoRepository,GrupoRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+using AlmoxarifadoDomain.Models;
+using AlmoxarifadoInfrastructure.Data.Interfaces;
+using AlmoxarifadoInfrastructure.Data.Repositories;
+using AlmoxarifadoServices;
+using Microsoft.EntityFrameworkCore;
+using static AlmoxarifadoInfrastructure.Data.Repositories.ConexaoBancoRepository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddSingleton<PrimeiraConexao>(sp => new PrimeiraConexao(builder.Configuration));
+builder.Services.AddSingleton<ReplicaConexao>(sp => new ReplicaConexao(builder.Configuration));
+
+builder.Services.AddSingleton<AlmoxarifadoInfrastructure.Data.ConexaoBancoService>();
+
+builder.Services.AddDbContext<xAlmoxarifadoContext>((serviceProvider, options) =>
+{
+    var conexaoBancoService = serviceProvider.GetRequiredService<AlmoxarifadoInfrastructure.Data.ConexaoBancoService>();
+    var connectionString = conexaoBancoService.GetConnectionString();
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddScoped<xAlmoxarifadoContext>();
+
+builder.Services.AddScoped<IItensNotaRepository, ItensNotaRepository>();
+builder.Services.AddScoped<ItensNotaService>();
+builder.Services.AddScoped<IItensRequisicaoRepository, ItensRequisicaoRepository>();
+builder.Services.AddScoped<ItensRequisicaoService>();
+builder.Services.AddScoped<INotaFiscalRepository, NotasFiscaisRepository>();
+builder.Services.AddScoped<NotaFiscalService>();
+builder.Services.AddScoped<IRequisicoesRepository, RequisicoesRepository>();
+builder.Services.AddScoped<RequisicaoService>();
+builder.Services.AddScoped<IEstoqueRepository, EstoqueRepository>();
+builder.Services.AddScoped<EstoqueService>();
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
